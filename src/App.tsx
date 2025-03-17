09812c927b941c75b78c8b69e8979018cc017f91
Master-AMW <lgnjasonn@gmail.com>
2025-03-16 20:55:16 -0700

    Minor changes to gap of skills and their background color

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import About from "./components/About";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contacts from "./components/Contacts";
 
 import { useRef } from "react";
 
 import "./App.css";
-import Contacts from "./components/Contacts";
 
 function App() {
 	const navElement = useRef<HTMLElement>(null);
